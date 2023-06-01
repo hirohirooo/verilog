acba2141118df3de186b5bcc9dfa60d0b924b999
hirohirooo <126838535+hirohirooo@users.noreply.github.com>
2023-06-01 15:00:44 +0900

    edit 7_report_3

--- 7_report_3/fifo.sv
+++ 7_report_3/fifo.sv
@@ -1,32 +1,47 @@
-//fifoをリセットなしFFで実装する
 module fifo(in, we, full, out, re, empty, clk, rst);
 	input [7:0] in;
 	input we;
 	output logic full;
-	output logic [7:0] out;
+	output [7:0] out;
 	input re;
 	output logic empty;
 	input clk, rst;
 	logic [7:0] mem[7:0];
-	logic [2:0] head = 0, tail = 0, headi;
+	logic [2:0] head, tail, headi;
+	logic [7:0] out;
 
-	always_ff @(negedge clk) begin
-		if(we && ~full) begin
-			head <= head + 1;
-			mem[head] <= in;
-		end 
-		if(re && ~empty) begin
-			tail <= tail + 1;
-			out <= mem[tail];
+	always_ff @(posedge clk) begin
+		if(rst) begin
+			head <= 0;
+			tail <= 0;
+		end else begin
+      case({re,we,~full,~empty})
+        4'b1001,4'b1011,4'b1101:begin
+          tail <= tail + 1;
+          out <= mem[tail];
+        end
+        4'b0110,4'b0111,4'b1110:begin
+          head <= head + 1;
+          mem[head] <= in;
+          out <= 0;
+        end
+        4'b1111:begin
+          tail <= tail + 1;
+          out <= mem[tail];
+          head <= head + 1;
+          mem[head] <= in;
+        end          
+        default:begin
+          out <= 0;
+        end
+      endcase
 		end
-		else out <= 0;
 	end
 
 	always_comb begin
 		headi = head + 1;
-		if(head == tail) empty = 1'b1;
-		else empty = 1'b0;
-		if(headi == tail) full = 1'b1;
-		else full = 1'b0;
+    empty = (head == tail);
+		full = (headi == tail);
 	end
+
 endmodule
